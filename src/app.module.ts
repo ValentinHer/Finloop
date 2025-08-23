42b730855b347533cb33d73be79ec3e1368ec422
ValentinHer <valentinhernandezhernandez18@gmail.com>
2025-08-22 18:50:28 -0600

    [ADD] validación para el archivo .env agregado, configuración de bd añadido

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,32 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { RoleModule } from './role/role.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { validate } from './config/env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate,
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true
+      }),
+      inject: [ConfigService]
+    }),
+    UserModule, RoleModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/config/env.validation.ts
@@ -0,0 +1,42 @@
+import { plainToInstance } from "class-transformer";
+import { IsEnum, IsNumber, IsString, Min, validateSync } from "class-validator";
+
+enum Enviroment {
+    DEVELOPMENT = "development",
+    PRODUCTION = "production",
+    TEST = "test"
+}
+
+class EnviromentVariables {
+    @IsEnum(Enviroment)
+    NODE_ENV: Enviroment;
+
+    @IsString()
+    DB_HOST: string;
+
+    @IsNumber()
+    @Min(0)
+    DB_PORT: number;
+
+    @IsString()
+    DB_USER: string;
+
+    @IsString()
+    DB_PASSWORD: string;
+
+    @IsString()
+    DB_NAME: string;
+}
+
+export function validate(config: Record<string, unknown>) {
+    const validateConfig = plainToInstance(
+        EnviromentVariables,
+        config,
+        {enableImplicitConversion: true},
+    )
+    const errors = validateSync(validateConfig, { skipMissingProperties: false });
+
+    if (errors.length > 0) throw new Error(errors.toString());
+
+    return validateConfig;
+}
